d451bd0fb5f48d939c988ffebbc4519e7fd9c713
ian douglas <ian.douglas@iandouglas.com>
2018-04-02 12:05:03 -0600

    authentication lesson

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get '/movies', to: 'movies#index'
+  root 'welcome#index'
 
   resources :directors, shallow: true do
     resources :movies
   end
   resources :movies, only: [:index]
   resources :actors
+
+  resources :users, only: [:new, :create, :show]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323170923) do
+ActiveRecord::Schema.define(version: 20180402154638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20180323170923) do
     t.datetime "updated_at", null: false
     t.bigint "director_id"
     t.index ["director_id"], name: "index_movies_on_director_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "actor_movies", "actors"
