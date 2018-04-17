6a927f273fde978326501f2ff3c856895c1a5c84
Margaret Williford <margaretwilliford4@gmail.com>
2018-04-17 09:38:52 -0600

    second user story

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,5 @@
 class GenresController < ApplicationController
+  before_action :require_admin, only: %i[create]
 
   def index
     @genres = Genre.all
@@ -14,10 +15,18 @@ class GenresController < ApplicationController
     end
   end
 
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
   private
 
   def genre_params
     params.require(:genre).permit(:name)
+  end
+
+  def require_admin
+    render file: '/public/404' unless current_admin?
   end
 
 end
