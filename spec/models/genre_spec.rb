d7b294cc1646c6fdd2440a867c0ca3e1af42b4cb
VadLusk <AdLusk@gmail.com>
2018-04-17 09:26:26 -0600

    Add movie genres model. Pass model tests.

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -5,12 +5,12 @@ describe Genre do
     it 'has many movie_genres' do
       genre = Genre.create!
 
-      expect(genre).to belong_to(:movie_genre)
+      expect(genre).to have_many(:movie_genres)
     end
     it 'has many movies through movie_genres' do
       genre = Genre.create!
 
-      expect(genre).to have_many(:movie).through(:movie_genres)
+      expect(genre).to have_many(:movies).through(:movie_genres)
     end
   end
 end
