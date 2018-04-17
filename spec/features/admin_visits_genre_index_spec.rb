99a4e70f9e8a4f77c2c6af922716116132c08ae2
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-17 09:35:39 -0600

    Adds test for second part of user story 1

--- spec/features/admin_visits_genre_index_spec.rb
+++ spec/features/admin_visits_genre_index_spec.rb
@@ -18,5 +18,16 @@ describe 'admin' do
       expect(current_path).to eq(genres_path)
       expect(page).to have_content('Incredikelly')
     end
+    it 'sees all genres' do
+      admin = User.create(username: 'name', password: 'password', role: 'admin')
+      genre1 = Genre.create(name: 'bad stuff')
+      genre2 = Genre.create(name: 'good stuff')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit genres_path
+
+      expect(page).to have_content(genre1.name)
+      expect(page).to have_content(genre2.name)
+    end
   end
 end
