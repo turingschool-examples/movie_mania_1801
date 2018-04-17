5d33662bf2d0ee3a0410b15e67471ef3662dd08d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-17 09:08:04 -0600

    Genre spec

---
+++ spec/features/admin/admin_can_create_genres_spec.rb
@@ -0,0 +1,8 @@
+# As an admin user,
+# When I visit the genre index,
+# I can see a form to create new genres,
+# When I fill out the form with the genre's name,
+# And click submit,
+# I see a list of genres I have created.
+
+require 'rails_helper'
