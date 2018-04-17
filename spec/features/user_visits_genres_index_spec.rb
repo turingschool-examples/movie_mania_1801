d46f7b289123783ff976da307ccc6dd3ce60ce02
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-17 10:07:57 -0600

    Fixes test to not be a logged in user

--- spec/features/user_visits_genres_index_spec.rb
+++ spec/features/user_visits_genres_index_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 describe 'unregistered user' do
   describe 'visits genre index' do
     it 'sees all genres' do
-      user = User.create(username: 'name', password: 'password', role: 'default')
       genre1 = Genre.create(name: 'bad stuff')
       genre2 = Genre.create(name: 'good stuff')
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit genres_path
 
@@ -14,19 +12,15 @@ describe 'unregistered user' do
       expect(page).to have_content(genre2.name)
     end
     it 'does not see the form to create genres' do
-      user = User.create(username: 'name', password: 'password', role: 'default')
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit genres_path
 
       expect(page).to_not have_content('Create a new genre:')
     end
     it 'sees that genre names are links' do
-      user = User.create(username: 'name', password: 'password', role: 'default')
       genre1 = Genre.create(name: 'bad stuff')
       Genre.create(name: 'good stuff')
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit genres_path
 
