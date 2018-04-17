4cbd5a31473950e28436dcf1c142c968c1202c74
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-17 17:37:19 -0600

    Adds test for genre show displaying average rating of films in that genre and adds html to the show

--- spec/features/user_sees_movies_on_genre_show_spec.rb
+++ spec/features/user_sees_movies_on_genre_show_spec.rb
@@ -5,8 +5,8 @@ describe 'unregistered user' do
     it 'clicks on actions' do
       director = Director.create(name: 'Dougie B')
       genre = Genre.create(name: 'Action')
-      movie1 = genre.movies.create(title: 'Guardians of the Galaxy', description: 'People destroy the galaxy. People stop it', director_id: director.id, rating: 4)
-      movie2 = genre.movies.create(title: 'Suicide Squad', description: 'People suicide themselves. No one stops it', director_id: director.id, rating: 3)
+      genre.movies.create(title: 'Guardians of the Galaxy', description: 'People destroy the galaxy. People stop it', director_id: director.id, rating: 4)
+      genre.movies.create(title: 'Suicide Squad', description: 'People suicide themselves. No one stops it', director_id: director.id, rating: 2)
 
       visit genres_path
 
@@ -16,6 +16,7 @@ describe 'unregistered user' do
       expect(page).to have_content('Action')
       expect(page).to have_content('Guardians of the Galaxy')
       expect(page).to have_content('Suicide Squad')
+      expect(page).to have_content('Average rating of movies in this genre: 3')
     end
   end
 end
