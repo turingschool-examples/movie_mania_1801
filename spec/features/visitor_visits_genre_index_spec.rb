7f6340071b78bb1ebb5c89f1aa2a03d94d478260
VadLusk <AdLusk@gmail.com>
2018-04-17 09:48:31 -0600

    Add test for second story.

---
+++ spec/features/visitor_visits_genre_index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'Visitor' do
+  context 'visits the genre index' do
+    scenario 'and cannot see form for new genre' do
+      genre_1 = Genre.create!(name: "classic")
+      genre_2 = Genre.create!(name: "horror")
+      genre_3 = Genre.create!(name: "western")
+      visit genres_path
+
+      expect(page).to_not have_button("submit")
+      expect(page).to have_link(genre_1.name)
+      expect(page).to have_link(genre_2.name)
+      expect(page).to have_link(genre_3.name)
+
+      click_on genre_1.name
+
+      expect(current_path).to eq(genre_path(genre_1))
+    end
+  end
+end
