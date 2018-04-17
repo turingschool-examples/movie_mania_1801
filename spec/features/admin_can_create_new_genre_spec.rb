13197220b71f9478c7f8f4575afba0552d520bcf
hmesander <haleymesander@gmail.com>
2018-04-17 09:08:13 -0600

    Adds test that admin can create a new genre

---
+++ spec/features/admin_can_create_new_genre_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "admin" do
+  scenario "can create a new genre" do
+    admin = User.create!(username: "admin", password: "password", role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit admin_genres_path
+
+    fill_in "Name", with: "SciFi"
+    click_on "Submit"
+
+    expect(page).to have_content("SciFi")
+  end
+end
