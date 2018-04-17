96c2171d1dd5e8198e634a1c7031718d46fbbeed
agpiermarini <piermarini.andrew@gmail.com>
2018-04-17 10:10:41 -0600

    Add relationships and corresponding model tests. Add genres to movie show

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Movie, type: :model do
   describe 'relationships' do
     it {is_expected.to have_many(:actors).through(:actor_movies)}
+    it {is_expected.to have_many(:genres).through(:movie_genres)}
   end
 end
