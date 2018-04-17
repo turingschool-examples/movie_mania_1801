b33785b1963c02fdded9e723d5135a5a835f8c3d
adam-conway <adam.n.conway@gmail.com>
2018-04-17 09:53:47 -0600

    Added test for user story 4

--- spec/features/visitor_sees_genre_index_spec.rb
+++ spec/features/visitor_sees_genre_index_spec.rb
@@ -22,7 +22,7 @@ describe "visitor" do
     MovieGenre.create!(movie_id: movie1.id, genre_id: genre1.id)
     MovieGenre.create!(movie_id: movie1.id, genre_id: genre2.id)
     MovieGenre.create!(movie_id: movie1.id, genre_id: genre3.id)
-    
+
     visit movies_path
     click_on movie1.title
 
@@ -32,5 +32,21 @@ describe "visitor" do
     expect(page).to have_content(genre2.name)
     expect(page).to have_content(genre3.name)
     expect(page).to have_content(movie1.rating)
+  end
+  scenario "can go to genre show page and see all movies with that genre" do
+    genre1 = Genre.create!(name: "Action")
+    director = Director.create!(name: 'Wakowski Sisters')
+    movie1 = Movie.create!(director_id: director.id, title: "Guardians of the Galaxy", description: "Practically perfect in every way", rating: 5)
+    movie2 = Movie.create!(director_id: director.id, title: "Suicide Squad", description: "Practically perfect in every way", rating: 15)
+    MovieGenre.create!(movie_id: movie1.id, genre_id: genre1.id)
+    MovieGenre.create!(movie_id: movie2.id, genre_id: genre1.id)
+    average = (movie1.rating + movie2.rating) / 2
+
+    visit genres_path
+
+    expect(page).to have_content(genre1.name)
+    expect(page).to have_content(movie1.title)
+    expect(page).to have_content(movie2.title)
+    expect(page).to have_content("Average rating: #{average}")
   end
 end
