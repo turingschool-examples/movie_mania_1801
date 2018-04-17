f4f918a95d51106574d673211a066175dc7a7a8b
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-17 17:13:12 -0600

    Moved genre creation to admin controller

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,19 @@
+# Admin genre controller
+class Admin::GenresController < ApplicationController
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "#{@genre.name} added successfully!"
+      redirect_to genres_path
+    else
+      flash[:error] = 'There was an error saving that genre.'
+      render :'genres/index'
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -7,20 +7,4 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id])
   end
-
-  def create
-    @genre = Genre.new(genre_params)
-    if !@genre.save
-      flash[:error] = 'There was an error saving that genre.'
-      redirect_to genres_path
-    else
-      flash[:notice] = "#{@genre.name} added successfully!"
-      redirect_to genres_path
-    end
-  end
-
-  private
-  def genre_params
-    params.require(:genre).permit(:name)
-  end
 end
