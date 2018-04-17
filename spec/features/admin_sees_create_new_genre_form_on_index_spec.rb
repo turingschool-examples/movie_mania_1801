62f48df6b4fa3bccbade53fa3af1bf2073fc5829
adam-conway <adam.n.conway@gmail.com>
2018-04-17 09:08:12 -0600

    Added test for admin creating new genre

---
+++ spec/features/admin_sees_create_new_genre_form_on_index_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "admin" do
+  scenario "can see all genres" do
+    admin = User.create!(username: "admin", password: "password", role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit genres_path
+
+    fill_in :name, with: "Horror"
+    click_on 'Create Genre'
+
+    expect(page).to have_content("Horror")
+  end
+end
