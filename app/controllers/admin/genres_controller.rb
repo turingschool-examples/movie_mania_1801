6c215003ef36860c6fca181139e05f4c1a1e05f1
Lihkin <shniks@gmail.com>
2018-04-17 09:22:10 -0600

    Created feature test for the admin genre new.

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,7 @@
+class Admin::GenresController < Admin::BaseController
+
+  def index
+
+  end
+
+end
---
+++ spec/features/admin_can_create_new_genre_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe "As an admin" do
+  describe "When I visit the genre index"
+    describe "I fill out a form to create a new genre and click submit" do
+      scenario "I can see the new genres along with a list of genres in the genres index page" do
+        admin = User.create!(username: "admin", password: "password", role: 1)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+        visit admin_genres_path
+
+        click_on 'Create Genre'
+
+        fill_in 'genre[name]', with: 'Action'
+
+        expect(current_path).to eq(admin_genres_path)
+        expect(page).to have_content("Admin Genres")
+        expect(page).to have_content("Action")
+  end
+end
