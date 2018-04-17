9b5c81d58d0bf325601a27834726cc10e9811232
Lihkin <shniks@gmail.com>
2018-04-17 10:48:10 -0600

    Passing test for user story 4. Completed all user stories.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,8 @@ class Genre < ApplicationRecord
   validates_presence_of :name
   has_many :movie_genres
   has_many :movies, through: :movie_genres
+
+  def average_rating
+    movies.average(:rating)
+  end
 end
