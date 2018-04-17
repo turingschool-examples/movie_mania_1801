eccd5f1a2ff1b050c9bc31d73a920597601acca4
alexcutschall <alexander.cutschall@gmail.com>
2018-04-17 10:07:49 -0600

    changing small capitalizaiton

--- spec/features/unregistered_user_visits_movie_index_spec.rb
+++ spec/features/unregistered_user_visits_movie_index_spec.rb
@@ -7,12 +7,12 @@ describe "as an unregistered user" do
     genre1 = movie.genres.create!(title: "Action")
     genre2 = movie.genres.create!(title: "Adventure")
     genre3 = movie.genres.create!(title: "Sci-Fi")
-    
+
     visit movies_path
     click_on "Guardians of the Galaxy"
 
     expect(page).to have_content(movie.title)
     expect(page).to have_content(movie.rating)
-    expect(page).to have_content("Genres for this movie: #{genre1.title}, #{genre2.title}, #{genre3.title}")
+    expect(page).to have_content("Genres for this Movie: #{genre1.title}, #{genre2.title}, #{genre3.title}")
   end
 end
