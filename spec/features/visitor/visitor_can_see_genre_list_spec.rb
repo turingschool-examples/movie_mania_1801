5a1fc0c14fd2b0d4752cd26d96214f608af9c787
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-17 09:49:36 -0600

    Visitor can see genre list

--- spec/features/visitor/visitor_can_see_genre_list_spec.rb
+++ spec/features/visitor/visitor_can_see_genre_list_spec.rb
@@ -8,10 +8,37 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   context 'when I visit the genres index' do
+    before(:all) do
+      DatabaseCleaner.clean
+      @genres = create_list(:genre, 3)
+    end
+
+    after(:all) do
+      DatabaseCleaner.clean
+    end
+
     scenario 'I cannot see the create form' do
       visit genres_path
 
       expect(page).to_not have_xpath('//form')
+    end
+
+    scenario 'I can see a list of all genres' do
+      visit genres_path
+
+      @genres.each do |genre|
+        expect(page).to have_content(genre.name)
+      end
+    end
+
+    scenario 'I can travel to a specific genre show page' do
+      visit genres_path
+
+      genre = @genres.sample
+      click_on genre.name
+
+      expect(current_path).to eq(genre_path(genre))
+      expect(page).to have_content(genre.name)
     end
   end
 end
