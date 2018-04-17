276312d8779f11127dc105d2c6c79a3e507a9255
Margaret Williford <margaretwilliford4@gmail.com>
2018-04-17 09:23:25 -0600

    first user story

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,23 @@
+class GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def create
+    genre = Genre.new(genre_params)
+    if genre.save
+      redirect_to genres_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403152919) do
+ActiveRecord::Schema.define(version: 20180417151424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20180403152919) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "movies", force: :cascade do |t|
