1be5d8e5477eaeaac8998fb26b014ed6e93dcc10
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-17 10:25:25 -0600

    Finished model testing for average movie reviews.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,8 @@ class Genre < ApplicationRecord
 
   has_many :movie_genres
   has_many :movies, through: :movie_genres
+
+  def average_movie_rating
+    movies.average(:rating)
+  end
 end
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe Genre, type: :model do
    it { should have_many(:movie_genres) }
    it { should have_many(:movies) }
  end
+
+ describe 'methods' do
+   it 'should give average' do
+     director = Director.create!(name: 'Keller Man')
+     genre_1 = Genre.create!(name: 'Action')
+     movie_1 = genre_1.movies.create!(director_id: director.id, title: "Guardians of the Galaxy", description: "Practically ridiculous", rating: 4)
+     movie_2 = genre_1.movies.create!(director_id: director.id, title: "Suicide Squad", description: "Nonsense", rating: 2)
+
+
+     expect(genre_1.average_movie_rating).to eq(3)
+   end
+ end
 end
