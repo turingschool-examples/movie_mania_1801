1ec9488e505cbdb1a982fc0087c31104225358c4
Margaret Williford <margaretwilliford4@gmail.com>
2018-04-17 09:50:40 -0600

    relationship working

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 describe Movie, type: :model do
   describe 'relationships' do
-    it 'has many genres' do
-      movie = Movie.create(title: 'ksadjhf', description: 'kajsdhfasdf')
-      expect(movie).to respond_to(genres)
-    end
+    it {is_expected.to have_many(:genres).through(:genre_movies)}
     it {is_expected.to have_many(:actors).through(:actor_movies)}
   end
 end
