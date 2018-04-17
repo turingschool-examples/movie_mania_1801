af8638d32373bd5675524c09fb0b206cea1ef079
alexcutschall <alexander.cutschall@gmail.com>
2018-04-17 10:00:29 -0600

    failing tests because of multiples on page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
   has_many :actor_movies
   has_many :actors, through: :actor_movies
+
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
   belongs_to :director
-  has_many :genres
 end
--- spec/features/unregistered_user_visits_movie_index_spec.rb
+++ spec/features/unregistered_user_visits_movie_index_spec.rb
@@ -2,10 +2,17 @@ require "rails_helper"
 
 describe "as an unregistered user" do
   scenario "they visit the movies index page" do
-    director = Director.create(name: "James Gunn")
-    movie = Movie.create(title: "Guardians of the Galaxy", description: "Great Movie", rating: 5, director_id: director.id)
-    genre1 = movie.genres.create(title: "Action")
-    genre1 = movie.genres.create(title: "Adventure")
-    genre1 = movie.genres.create(title: "Sci-Fi")
+    director = Director.create!(name: "James Gunn")
+    movie = Movie.create!(title: "Guardians of the Galaxy", description: "Great Movie", rating: 5, director_id: director.id)
+    genre1 = movie.genres.create!(title: "Action")
+    genre2 = movie.genres.create!(title: "Adventure")
+    genre3 = movie.genres.create!(title: "Sci-Fi")
+    
+    visit movies_path
+    click_on "Guardians of the Galaxy"
+
+    expect(page).to have_content(movie.title)
+    expect(page).to have_content(movie.rating)
+    expect(page).to have_content("Genres for this movie: #{genre1.title}, #{genre2.title}, #{genre3.title}")
   end
 end
