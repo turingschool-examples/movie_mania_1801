de826ab6dbf7c935335e98c5353a16e1af403c29
Lihkin <shniks@gmail.com>
2018-04-17 10:20:10 -0600

    Added rating attribute to the movies table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417152725) do
+ActiveRecord::Schema.define(version: 20180417161943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20180417152725) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "director_id"
+    t.integer "rating"
     t.index ["director_id"], name: "index_movies_on_director_id"
   end
 
