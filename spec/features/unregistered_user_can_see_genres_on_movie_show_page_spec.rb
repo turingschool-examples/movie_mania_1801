e145078c438f1afd8702de8c001a0fae8a711707
hmesander <haleymesander@gmail.com>
2018-04-17 09:51:00 -0600

    Adds test for movie show page to have associated genres

---
+++ spec/features/unregistered_user_can_see_genres_on_movie_show_page_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "unregistered user sees genres associated with movie" do
+  context "they visit the movie index and click on a movie title" do
+    it "displays all genres which are associated with that movie" do
+      director = Director.create!(name: "Someone!")
+      movie = director.movies.create!(title: "Guardians of the Galaxy", description: "Owls, maybe?", rating: 3)
+      genre1 = movie.genres.create!(name: "Drama")
+      genre2 = movie.genres.create!(name: "SciFi")
+      genre3 = movie.genres.create!(name: "Comedy")
+
+      visit movies_path
+
+      click_on movie.title
+
+      expect(page).to have_content(movie.name)
+      expect(page).to have_content("Genres for this Movie:")
+      expect(page).to have_content(genre1.name)
+      expect(page).to have_content(genre2.name)
+      expect(page).to have_content(genre3.name)
+      expect(page).to have_content("Movie Rating: #{movie.rating}")
+    end
+  end
+end
