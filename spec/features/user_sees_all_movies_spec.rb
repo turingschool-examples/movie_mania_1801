31ec8e6c1c6f32143f44f6519600c5c278feba5b
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-17 09:57:45 -0600

    Starting user story 3.

--- spec/features/user_sees_all_movies_spec.rb
+++ spec/features/user_sees_all_movies_spec.rb
@@ -15,4 +15,28 @@ describe "user sees all movies" do
       expect(page).to have_content(movie_2.description)
     end
   end
+
+  describe "a visitor can link to movie show page" do
+    context "to see all the genres for that movie" do
+      it "can see multiple genres" do
+        director = Director.create!(name: 'Wakowski Sisters')
+        movie_1 = Movie.create!(director_id: director.id, title: "Guardians of the Galaxy", description: "Practically perfect")
+        genre_1 = movie_1.genres.create!(name: 'Action')
+        genre_2 = movie_1.genres.create!(name: 'Adventure')
+        genre_3 = movie_1.genres.create!(name: 'Sci-Fi')
+
+        visit movies_path
+
+        click_link movie_1.title
+
+        expect(current_path).to eq(movie_path(movie_1))
+
+        expect(page).to have_content(movie_1.title)
+        expect(page).to have_content(movie_1.description)
+        expect(page).to have_content(genre_1.name)
+        expect(page).to have_content(genre_2.name)
+        expect(page).to have_content(genre_3.name)
+      end
+    end
+  end
 end
