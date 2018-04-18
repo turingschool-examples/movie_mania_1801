fbdeb51f72911431b15012030bfb0530104f653d
Lihkin <shniks@gmail.com>
2018-04-17 19:04:56 -0600

    Created additional test to test flash message when it cannot create a genre.

--- spec/features/admin/admin_can_create_new_genre_spec.rb
+++ spec/features/admin/admin_can_create_new_genre_spec.rb
@@ -19,6 +19,20 @@ describe "As an admin" do
         expect(current_path).to eq(genres_path)
         expect(page).to have_content("Action")
       end
+
+      scenario "I should get a flash message if it fails to create a genre" do
+        admin = User.create!(username: "admin", password: "password", role: 1)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+        visit genres_path
+
+        click_on 'Create Genre'
+        fill_in 'genre[name]', with: ""
+        click_on 'Create Genre'
+
+        expect(current_path).to eq(genres_path)
+        expect(page).to have_content('Genre could not be created. Please try again!')
+      end
     end
   end
 end
