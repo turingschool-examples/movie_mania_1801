6038aaa7b32ab29f356b19a884c955d9809fdbd9
Lihkin <shniks@gmail.com>
2018-04-17 18:31:36 -0600

    Created namespace for admin for genre#show

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :categories, only: [:index]
-    resources :genres, only: [:index, :new, :create]
+    resources :genres, only: [:create]
   end
 
   get '/login', to: 'sessions#new'
