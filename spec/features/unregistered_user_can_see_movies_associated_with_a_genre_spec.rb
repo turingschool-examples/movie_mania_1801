1c4b87f229c96b35670f4475faebe96507e2968a
hmesander <haleymesander@gmail.com>
2018-04-18 00:38:02 -0600

    Adds string interpolation for average movie rating in feature test

--- spec/features/unregistered_user_can_see_movies_associated_with_a_genre_spec.rb
+++ spec/features/unregistered_user_can_see_movies_associated_with_a_genre_spec.rb
@@ -7,14 +7,14 @@ describe "unregistered user sees movies associated with genre" do
       director = Director.create!(name: "Someone!")
       movie1 = genre.movies.create!(title: "Guardians of the Galaxy", description: "Owls, maybe?", rating: 3, director_id: director.id)
       movie2 = genre.movies.create!(title: "Suicide Squad", description: "Doesn't sound good...", rating: 2, director_id: director.id)
-
+      average_rating = (movie1.rating + movie2.rating)/2
       visit genres_path
 
       click_on genre.name
 
       expect(page).to have_content(movie1.title)
       expect(page).to have_content(movie2.title)
-      expect(page).to have_content("Average Movie Rating: 2.5")
+      expect(page).to have_content("Average Movie Rating: #{average_rating}")
     end
   end
 end
