5f7380f7eaad2ff1a6d2fdb310991f7237be53b0
hmesander <haleymesander@gmail.com>
2018-04-18 00:34:52 -0600

    Adds validation model test that genre must have a name and validation on the genre model

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -4,4 +4,8 @@ describe Genre, type: :model do
   describe 'relationships' do
     it {is_expected.to have_many(:movies).through(:movie_genres)}
   end
+
+  describe 'validates' do
+    it { validate_presence_of :name }
+  end
 end
