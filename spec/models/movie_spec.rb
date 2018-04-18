a630668b99ce11eb474ff3a936b8b85ba4684636
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-17 21:27:56 -0600

    Needed a base controller too, and added movie validation testing.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe Movie, type: :model do
+  describe 'validations' do
+    it { validate_presence_of :director_id }
+    it { validate_presence_of :title }
+    it { validate_presence_of :description }
+    it { validate_presence_of :rating }
+  end
   describe 'relationships' do
     it {is_expected.to have_many(:actors).through(:actor_movies)}
     it { should have_many(:movie_genres) }
