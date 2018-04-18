c31cc1ebf4efe6a9b0f7abcb69aa1ec2ddd2d438
Evan Wheeler <anon0mys87@gmail.com>
2018-04-18 12:57:05 -0600

    Adds sad path testing for creating genre, and validation for genre name

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,6 @@
 class Genre < ApplicationRecord
+  validates_presence_of :name
+  
   has_many :movie_genres
   has_many :movies, through: :movie_genres
 
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Genre do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+  
   describe 'relationships' do
     it { should have_many(:movie_genres) }
     it { should have_many(:movies) }
