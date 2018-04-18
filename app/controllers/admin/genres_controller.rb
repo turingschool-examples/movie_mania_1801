87b0b1af4b0e9016c9fbfa76085caa462c13876d
Megan Arellano <ssciolist@gmail.com>
2018-04-17 20:16:02 -0600

    brings tests back to passing with admin genres controller + form_for change

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,15 @@
+class Admin::GenresController < Admin::BaseController
+  def create
+    genre = Genre.new(genre_params)
+    if genre.save
+      redirect_to genres_path
+    else
+      flash[:notice] = "Oops something went wrong"
+    end
+  end
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
