eee51779aa535fa62a4a0a299b7848ba44614beb
hmesander <haleymesander@gmail.com>
2018-04-18 00:32:36 -0600

    Adds feature test for sad path

--- spec/features/admin_can_create_new_genre_spec.rb
+++ spec/features/admin_can_create_new_genre_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "admin" do
-  scenario "can create a new genre if all genre information is present" do
+  it "can create a new genre if genre name is given" do
     admin = User.create!(username: "admin", password: "password", role: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
@@ -12,5 +12,17 @@ describe "admin" do
     click_on "Create Genre"
 
     expect(page).to have_content("SciFi")
+  end
+
+  it "cannot create a new genre if genre name is missing" do
+    admin = User.create!(username: "admin", password: "password", role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit genres_path
+
+    click_on "Create Genre"
+
+    expect(page).to have_content("Your genre was not successfully created.")
   end
 end
