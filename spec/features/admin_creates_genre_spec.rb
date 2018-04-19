9eaff02ec794578da653b448a23f80496df03bb4
VadLusk <AdLusk@gmail.com>
2018-04-18 18:07:08 -0600

    Add sad path test.

--- spec/features/admin_creates_genre_spec.rb
+++ spec/features/admin_creates_genre_spec.rb
@@ -12,6 +12,17 @@ describe 'Admin' do
       click_on 'submit'
 
       expect(page).to have_content('Horror')
+      expect(page).to have_content('Good job')
+    end
+    scenario 'and does not make a new genre because of poor usage' do
+      admin = User.create!(username: "adam", password: '12345', role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      visit genres_path
+
+      click_on 'submit'
+
+      expect(page).to have_content('DIR DDINR')
     end
   end
 end
